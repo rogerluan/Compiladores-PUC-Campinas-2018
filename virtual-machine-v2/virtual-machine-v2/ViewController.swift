6807928e1198ff54956f7d0c9ff37ab598f8c6c6
Roger Oba <rogerluan.oba@gmail.com>
2018-08-08 12:50:59 -0300

    Sketch first version of the user interface and link outlets/actions.

--- virtual-machine-v2/virtual-machine-v2/ViewController.swift
+++ virtual-machine-v2/virtual-machine-v2/ViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    @IBOutlet private var inputTextView: UITextView!
+    @IBOutlet private var outputTextView: UITextView!
+    @IBOutlet private var inputTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+
+    @IBAction func openFilePicker(_ sender: UIButton) {
+        // TODO: Open iCloud Drive picker
+    }
+
+    @IBAction func run(_ sender: UIButton) {
+        // TODO: guard against lack of file
+        // TODO: run the file
+        // TODO: Implement different run options (e.g. step by step, or free running)
+    }
+
+    @IBAction func pauseOrPlay(_ sender: UIButton) {
+        // TODO: Detect if the current state is pause, or play, and execute the respective function
     }
 }
