0d91e94b7323e33890e7c95e058d2f773a3d3e99
Roger Oba <rogerluan.oba@gmail.com>
2018-08-08 12:18:10 -0300

    Commit project stub.

---
+++ virtual-machine-v2/virtual-machine-v2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  virtual-machine-v2
+//
+//  Created by Roger Oba on 08/08/18.
+//  Copyright © 2018 Roger Oba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ virtual-machine-v2/virtual-machine-v2Tests/virtual_machine_v2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  virtual_machine_v2Tests.swift
+//  virtual-machine-v2Tests
+//
+//  Created by Roger Oba on 08/08/18.
+//  Copyright © 2018 Roger Oba. All rights reserved.
+//
+
+import XCTest
+@testable import virtual_machine_v2
+
+class virtual_machine_v2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
