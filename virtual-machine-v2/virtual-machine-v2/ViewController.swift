3aac6588cc8acfc200bc7df24398ed9a40d18d2d
Roger Oba <rogerluan.oba@gmail.com>
2018-08-22 12:37:26 -0300

    WIP.

--- virtual-machine-v2/virtual-machine-v2/ViewController.swift
+++ virtual-machine-v2/virtual-machine-v2/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 final class ViewController: UIViewController {
     @IBOutlet private var sourceTextView: UITextView!
     @IBOutlet private var instructionsTableView: InstructionsTableView!
+
     @IBOutlet private var inputTextView: UITextView!
     @IBOutlet private var outputTextView: UITextView!
     @IBOutlet private var inputTextField: UITextField!
@@ -27,9 +28,24 @@ final class ViewController: UIViewController {
         // TODO: guard against lack of file
         // TODO: run the file
         // TODO: Implement different run options (e.g. step by step, or free running)
+        read()
     }
 
     @IBAction func pauseOrPlay(_ sender: UIButton) {
         // TODO: Detect if the current state is pause, or play, and execute the respective function
+    }
+
+    /// Reads and processes instructions from the text view.
+    private func read() {
+        let instructions = Engine.shared.process(text: sourceTextView.text ?? "")
+        guard !instructions.isEmpty else { showFailureAlert(); return }
+        instructionsTableView.items = instructions
+    }
+
+    private func showFailureAlert() {
+        let alert = UIAlertController(title: NSLocalizedString("Failed to Compile", comment: ""), message: NSLocalizedString("The source code failed to compile or returned no instructions.", comment: ""), preferredStyle: .alert)
+        let okAction = UIAlertAction(title: NSLocalizedString("Ok", comment: ""), style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 }
