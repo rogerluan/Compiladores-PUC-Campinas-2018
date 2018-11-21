915ce0d9b84607c6d827f220c34f338a09b7bfec
Roger Oba <rogerluan.oba@gmail.com>
2018-11-21 13:05:03 -0200

    Implement symbol table and associated models and protocols.

---
+++ virtual-machine-v2/virtual-machine-v2/Type.swift
@@ -0,0 +1,17 @@
+//
+//  Type.swift
+//  virtual-machine-v2
+//
+//  Created by Roger Oba on 17/10/18.
+//  Copyright © 2018 Roger Oba. All rights reserved.
+//
+
+import Foundation
+
+/// The type of variables and function returns.
+///
+/// - Int: an integer type.
+/// - Bool: a boolean type.
+enum Type {
+    case int, bool
+}
