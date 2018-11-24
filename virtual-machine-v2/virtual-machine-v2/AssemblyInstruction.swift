166e795360fc0716824225adc8faa50279bde660
Roger Oba <rogerluan.oba@gmail.com>
2018-11-24 19:07:34 -0200

    Implement the CodeGenerator and a (temporary) AssemblyInstruction.

---
+++ virtual-machine-v2/virtual-machine-v2/AssemblyInstruction.swift
@@ -0,0 +1,120 @@
+//
+//  AssemblyInstruction.swift
+//  virtual-machine-v2
+//
+//  Created by Roger Oba on 21/11/18.
+//  Copyright © 2018 Roger Oba. All rights reserved.
+//
+
+import Foundation
+
+// FIXME: Merge Instruction into this (or vice versa)
+enum AssemblyInstruction : Hashable {
+    case loadConstant(constant: String)
+    case loadValue(index: Int)
+    case add
+    case subtract
+    case multiply
+    case divide
+    case invert
+    case and
+    case or
+    case negate
+    case compareLesserThan
+    case compareGreaterThan
+    case compareEqual
+    case compareDifferent
+    case compareLesserThanOrEqualTo
+    case compareGreaterThanOrEqualTo
+    case start
+    case halt
+    case assign(index: Int)
+    case jump(label: String)
+    case jumpIfFalse(label: String)
+    case null(label: String)
+    case read
+    case print
+    case alloc(memoryHead: Int, length: Int)
+    case dealloc(memoryHead: Int, length: Int)
+    case call(label: String)
+    case `return`
+    case returnFunction(memoryHead: Int?, length: Int?)
+
+    var opcode: String {
+        switch self {
+        case .loadConstant: return "LDC"
+        case .loadValue: return "LDV"
+        case .add: return "ADD"
+        case .subtract: return "SUB"
+        case .multiply: return "MULT"
+        case .divide: return "DIVI"
+        case .invert: return "INV"
+        case .and: return "AND"
+        case .or: return "OR"
+        case .negate: return "NEG"
+        case .compareLesserThan: return "CME"
+        case .compareGreaterThan: return "CMA"
+        case .compareEqual: return "CEQ"
+        case .compareDifferent: return "CDIF"
+        case .compareLesserThanOrEqualTo: return "CMEQ"
+        case .compareGreaterThanOrEqualTo: return "CMAQ"
+        case .start: return "START"
+        case .halt: return "HLT"
+        case .assign: return "STR"
+        case .jump: return "JMP"
+        case .jumpIfFalse: return "JMPF"
+        case .null: return "NULL"
+        case .read: return "RD"
+        case .print: return "PRN"
+        case .alloc: return "ALLOC"
+        case .dealloc: return "DALLOC"
+        case .call: return "CALL"
+        case .return: return "RETURN"
+        case .returnFunction: return "RETURNF"
+        }
+    }
+
+    var argument1: String? {
+        switch self {
+        case .loadConstant(let constant): return "\(constant)"
+        case .loadValue(let memoryIndex): return String(memoryIndex)
+        case .assign(let memoryIndex): return String(memoryIndex)
+        case .jump(let instructionPoint): return String(instructionPoint)
+        case .jumpIfFalse(let instructionPoint): return String(instructionPoint)
+        case .null(let label): return label
+        case .alloc(let memoryHead, _): return String(memoryHead)
+        case .dealloc(let memoryHead, _): return String(memoryHead)
+        case .returnFunction(let memoryHead, _): return memoryHead != nil ? String(memoryHead!) : nil
+        case .call(let instructionPoint): return String(instructionPoint)
+        case .add, .subtract, .multiply, .divide, .invert, .and, .or, .negate,
+             .compareLesserThan, .compareGreaterThan, .compareEqual, .compareDifferent,
+             .compareLesserThanOrEqualTo, .compareGreaterThanOrEqualTo, .start,
+             .halt, .read, .print, .return: return nil
+        }
+    }
+
+    var argument2: String? {
+        switch self {
+        case .alloc(_, let length): return String(length)
+        case .dealloc(_, let length): return String(length)
+        case .returnFunction(_, let length): return length != nil ? String(length!) : nil
+        case .loadConstant, .loadValue, .add, .subtract, .multiply, .divide,
+             .invert, .and, .or, .negate, .compareLesserThan, .compareGreaterThan,
+             .compareEqual, .compareDifferent, .compareLesserThanOrEqualTo,
+             .compareGreaterThanOrEqualTo, .start, .halt, .assign, .jump,
+             .jumpIfFalse, .null, .read, .print, .call, .return: return nil
+        }
+    }
+
+    var hasNoArguments: Bool {
+        return argument1 == nil && argument2 == nil
+    }
+
+    var hasStrictlyOneArgument: Bool {
+        return argument1 != nil && argument2 == nil
+    }
+
+    var hasStrictlyTwoArguments: Bool {
+        return argument1 != nil && argument2 != nil
+    }
+}
---
+++ virtual-machine-v2/virtual-machine-v2/Code Generation/CodeGenerator.swift
@@ -0,0 +1,82 @@
+//
+//  Codeswift
+//  virtual-machine-v2
+//
+//  Created by Roger Oba on 21/11/18.
+//  Copyright © 2018 Roger Oba. All rights reserved.
+//
+
+import Foundation
+
+final class CodeGenerator {
+    /// The end result, consisting of an executable assembly-like file.
+    var output: String = ""
+
+    var instructions: [Instruction] { return Engine.shared.process(text: output) }
+
+    var globalVariableCount = 0
+
+    func generateInstruction(_ instruction: AssemblyInstruction) {
+        if instruction.hasNoArguments {
+            output += instruction.opcode
+        } else if instruction.hasStrictlyOneArgument {
+            switch instruction {
+            case .null(let label): output += "\(label) \(instruction.opcode)"
+            default: output += "\(instruction.opcode) \(instruction.argument1!)"
+            }
+
+        } else if instruction.hasStrictlyTwoArguments {
+            output += "\(instruction.opcode) \(instruction.argument1!) \(instruction.argument2!)"
+        } else {
+            fatalError()
+        }
+        output += "\n"
+    }
+
+    func generateInstructionsForPostFixedExpression(_ expression: [TokenType], symbolTable: SymbolTable) {
+        for tokenType in expression {
+            switch tokenType {
+            case .leftParenthesis, .rightParenthesis: preconditionFailure()
+            case .operator(let `operator`):
+                switch `operator` {
+                case .unaryPlus: break // No-op
+                case .unaryMinus: generateInstruction(.negate)
+                case .not: generateInstruction(.invert)
+                case .multiplication: generateInstruction(.multiply)
+                case .division: generateInstruction(.divide)
+                case .sum: generateInstruction(.add)
+                case .subtraction: generateInstruction(.subtract)
+                case .greater: generateInstruction(.compareGreaterThan)
+                case .lesser: generateInstruction(.compareLesserThan)
+                case .greaterThanOrEqualTo: generateInstruction(.compareGreaterThanOrEqualTo)
+                case .lesserThanOrEqualTo: generateInstruction(.compareLesserThanOrEqualTo)
+                case .equal: generateInstruction(.compareEqual)
+                case .different: generateInstruction(.compareDifferent)
+                case .and: generateInstruction(.and)
+                case .or: generateInstruction(.or)
+                }
+            case .operand(let operand):
+                switch operand.symbol {
+                case .s_number: generateInstruction(.loadConstant(constant: operand.lexeme))
+                case .s_identifier:
+                    let entry = symbolTable.searchDeclaration(of: [ .variable, .function ], with: operand.lexeme, in: .global)
+                    if let variableEntry = entry as? VariableEntry {
+                        generateInstruction(.loadValue(index: variableEntry.index))
+                    } else if let functionEntry = entry as? FunctionEntry {
+                        generateInstruction(.call(label: functionEntry.label))
+                    } else {
+                        preconditionFailure()
+                    }
+                case .s_true: generateInstruction(.loadConstant(constant: "1"))
+                case .s_false: generateInstruction(.loadConstant(constant: "0"))
+                default: preconditionFailure("There shouldn't be a non-operand here.")
+                }
+            }
+        }
+    }
+
+    func reset() {
+        output = ""
+        globalVariableCount = 0
+    }
+}
